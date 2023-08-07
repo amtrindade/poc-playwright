095b589f99cdf4ca55d636686339050fa2466826
Antonio Trindade <atrindade@umov.me>
2023-08-07 17:51:37 -0300

    Initial project

---
+++ tests/example.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+test.only('check Java Page', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', {name: 'Get started' }).click();
+  await page.getByRole('button', {name: 'Node.js'}).hover();
+  await page.getByText('Java', {exact: true}).click();
+
+  await expect(page).toHaveURL(/.*intro/);
+  await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
+
+  const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+
+  await expect(page.getByText(javaDescription)).toBeVisible();
+});
